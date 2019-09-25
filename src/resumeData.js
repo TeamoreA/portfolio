1df4ffc0d11f2c18baa6ef6c8fd7f670c6d64a41
Timothy Kamau <teamorekamau97@gmail.com>
2019-09-25 14:45:43 +0300

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Timothy Kamau",
+    "role": "Fullstack web developer",
+    "linkedinId":"https://www.linkedin.com/in/timothy-kamau/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Backend APIs developer majorly, but also I have a great interest on frontend development and design",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/timothy-kamau/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/teamorea",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/MoreKamau",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Currently, I am a student at Kenyatta University who has worked with an International Company, Andela, for a period of one year. I am a self-taught and enthusiatic Engineer and each day I strive to learn something new.",
+    "address":"Kenya",
+    "website":"Email: teamorethestar@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Kenyatta University",
+        "specialization":"BSc.(Mathematics and Computer Science)",
+        "MonthOfPassing":"December",
+        "YearOfPassing":"2020",
+        "Achievements":"Active participation in mathematics contests and mathematics challenge activities"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Andela",
+        "specialization":"Software Development",
+        "MonthOfLeaving":"September",
+        "YearOfLeaving":"2019",
+        "Achievements":"Worked in teams and was able to develop various important applications for the company"
+      }
+    ],
+    "skillsDescription":"Programming skills",
+    "skills":[
+      {
+        "skillname":"Python/Django"
+      },
+      {
+        "skillname":"Python/Flask"
+      },
+      {
+        "skillname":"GraphQl"
+      },
+      {
+        "skillname":"Redis"
+      },
+      {
+        "skillname":"Postgres"
+      },
+      {
+        "skillname":"Angular"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Coverge",
+        "description":"Room mangement app",
+        "imgurl":"images/portfolio/console.jpg"
+      },
+      {
+        "name":"Megg",
+        "description":"A social hybrid app",
+        "imgurl":"images/portfolio/coffee.jpg"
+      },
+      {
+        "name":"Author's Haven",
+        "description":"An article publishing website",  
+        "imgurl":"images/portfolio/girl.jpg"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/into-the-light.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Timothy is a quick learner and an exceptional problem solver. While mentoring him, he was able to learn the Python/Flask/Postgres stack and deliver an API product in a very short time. I would count on him to push through and thrive in a difficult, uncertain environment.",
+        "name":"Allan Ibutiti"
+      },
+      {
+        "description":"He is a committed co-woker who dedicates his time and effort to the work given. He also ethical and easy to work with and always strives to get what's best for the organisation",
+        "name":"Darius Ndubi"
+      }
+    ]
+  }
+  
+  export default resumeData
